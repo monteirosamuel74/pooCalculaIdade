42fde1a61659a713e2c305e21032e1090453dad6
Samuel Monteiro <ss-monteiro@outlook.com>
2024-10-17 16:17:08 -0400

    Adicionar arquivos de projeto.

---
+++ pooCalculaIdade/Pessoa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    public class Pessoa
+    {
+		public Pessoa(String nomePessoa, int anoNascPessoa) 
+		{
+			this.nome = nomePessoa;
+			this.anoNascimento = anoNascPessoa;
+			this.CalcularIdade();
+		}
+		private int anoNascimento;
+
+		public int AnoNascimento
+		{
+			get { return anoNascimento; }
+			set { anoNascimento = value; }
+		}
+
+		private String nome;
+
+		public String Nome
+		{
+			get { return nome; }
+			set { nome = value.ToUpper(); }
+		}
+
+		private int idade;
+
+		public int Idade
+		{
+			get {
+                this.CalcularIdade();
+                return idade; }
+		}
+
+
+		public void ExibirDados()
+		{
+			Console.WriteLine("Nome: " + this.nome);
+			Console.WriteLine("Ano de nascimento: " + this.anoNascimento);
+			Console.WriteLine("Idade: " + this.Idade);
+		}
+
+		private void CalcularIdade()
+		{
+			DateTime data = DateTime.Now;
+			int ano = data.Year;
+			this.idade = ano - this.anoNascimento;
+		}
+	}
+}
---
+++ pooCalculaIdade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa idade");
+            Console.Write("Nome: ");
+            String nome = Console.ReadLine();
+            Console.Write("Ano de nascimento: ");
+            int anoNasc = Convert.ToInt32(Console.ReadLine());
+            Pessoa pessoa = new Pessoa(nome, anoNasc);
+            pessoa.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
